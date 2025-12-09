bedf2c2a21a465141377345c48f139a981e5a8c0
hiteshvaidya <hvaidya@usf.edu>
2025-12-08 22:37:47 -0500

    load layerIF values and run the log-Utility function. To-Do: Debug logUtility()

--- mdl/logUtility.py
+++ mdl/logUtility.py
@@ -4,11 +4,13 @@ import os
 import random
 import numpy as np
 import torch
+import math
+import util
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from datasets import load_dataset
-from kronfluence.task import CausalLanguageModelingTask
-from kronfluence.analyzer import Analyzer
+# from kronfluence.task import CausalLanguageModelingTask
+# from kronfluence.analyzer import Analyzer
 from peft import get_peft_model, LoraConfig 
 
 # from lib.eval import eval_ppl, eval_zero_shot
@@ -67,12 +69,12 @@ def get_el(layer_quality, c_l, lambda_value=1, beta=1, alpha=0.1, gamma=1):
     value = gamma * layer_quality**beta / (alpha + lambda_value * c_l)
     return max(value - 1.0, 0.0)
 
-def logUtility(budget: int, layerIF: list, layer_quality: list):
+def logUtility(budget: int, layer_quality: list):
     cost = 0
     lambda_value = 0.0
-    c_l = [1.0] * len(layerIF)
-    layer_wise_lambda = [0.0] * len(layerIF)
-    for layer in range(len(layerIF)):
+    c_l = [1.0] * len(layer_quality)
+    layer_wise_lambda = [0.0] * len(layer_quality)
+    for layer in range(len(layer_quality)):
         e_l = get_el(layer_quality[layer],
                     c_l[layer],
                     )
@@ -83,7 +85,7 @@ def logUtility(budget: int, layerIF: list, layer_quality: list):
     else:
         def get_g(lambda_value):
             value = 0
-            for layer in range(len(layerIF)):
+            for layer in range(len(layer_quality)):
                 value += c_l[layer] * get_el(layer_quality[layer],
                                              c_l[layer]) - budget
             return value
@@ -104,7 +106,7 @@ def logUtility(budget: int, layerIF: list, layer_quality: list):
         else:
             lambda_ub = lambda_avg
         
-        for layer in range(len(layerIF)):
+        for layer in range(len(layer_quality)):
             e_l[layer] = get_el(layer_quality[layer], c_l[layer])
         return lambda_avg, e_l
         
@@ -124,108 +126,114 @@ def main():
     per_layer_base_flops, per_layer_lora_flops_scaled = get_flops(flops_paths)
 
 
-    # Define the model name we want to load
-    model_name = 'mistralai/Mistral-7B-v0.1'
+    # # Define the model name we want to load
+    # model_name = 'mistralai/Mistral-7B-v0.1'
 
-    # Load the tokenizer
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # # Load the tokenizer
+    # tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    # Load the model
-    model = get_llm(model_name)
+    # # Load the model
+    # model = get_llm(model_name)
 
-    # Define the Task
-    # This tells kronfluence how to compute the loss for our model.
-    task = CausalLanguageModelingTask()
+    # # Define the Task
+    # # This tells kronfluence how to compute the loss for our model.
+    # task = CausalLanguageModelingTask()
 
-    # Create the Analyzer
-    analyzer = Analyzer(
-        analysis_name='glue-cola', # A name for this analysis run
-        model=model, # our PEFT model
-        task=task   # The causal LM task we defined
-    )
+    # # Create the Analyzer
+    # analyzer = Analyzer(
+    #     analysis_name='glue-cola', # A name for this analysis run
+    #     model=model, # our PEFT model
+    #     task=task   # The causal LM task we defined
+    # )
 
-    # load dataset
-    dataset = load_dataset('glue', 'cola')
+    # # load dataset
+    # dataset = load_dataset('glue', 'cola')
 
-    batch_size = 8
-    max_seq_length = 256
+    # batch_size = 8
+    # max_seq_length = 256
 
-    kwargs = {
-        'tokenizer': tokenizer,
-        'max_seq_length': max_seq_length
-    }
-    # Apply tokenize functoin to all splits of the dataset
-    tokenized_datasets = dataset.map(
-                            tokenize_function, 
-                            batched=True,
-                            fn_kwargs=kwargs # Pass the extra arguments here
-                        )
+    # kwargs = {
+    #     'tokenizer': tokenizer,
+    #     'max_seq_length': max_seq_length
+    # }
+    # # Apply tokenize functoin to all splits of the dataset
+    # tokenized_datasets = dataset.map(
+    #                         tokenize_function, 
+    #                         batched=True,
+    #                         fn_kwargs=kwargs # Pass the extra arguments here
+    #                     )
 
-    # set the format to PyTorch tensors
-    tokenized_datasets.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
+    # # set the format to PyTorch tensors
+    # tokenized_datasets.set_format(type='torch', columns=['input_ids', 'attention_mask', 'label'])
 
-    # Create training dataloader
-    train_dataloader = DataLoader(
-        tokenized_datasets['train'],
-        batch_size=batch_size,
-        collate_fn=task.collate_fn, # Use the task's special collate function
-        shuffle=True    # shuffle the training data
-    )
+    # # Create training dataloader
+    # train_dataloader = DataLoader(
+    #     tokenized_datasets['train'],
+    #     batch_size=batch_size,
+    #     collate_fn=task.collate_fn, # Use the task's special collate function
+    #     shuffle=True    # shuffle the training data
+    # )
 
-    valid_dataloader = DataLoader(
-        tokenized_datasets['valid'],
-        batch_size=batch_size,
-        collate_fn=task.collate_fn,
-        shuffle=False
-    )
+    # valid_dataloader = DataLoader(
+    #     tokenized_datasets['valid'],
+    #     batch_size=batch_size,
+    #     collate_fn=task.collate_fn,
+    #     shuffle=False
+    # )
 
-    # Compute and cache factors for influence function
-    # This is the most computationally heavy stop
-    # It goes through all training data and pre-computes the building blocks.
-    analyzer.fit_all_factors(
-        factors_name='IF_factors',
-        dataset=train_dataloader
-    )
+    # # Compute and cache factors for influence function
+    # # This is the most computationally heavy stop
+    # # It goes through all training data and pre-computes the building blocks.
+    # analyzer.fit_all_factors(
+    #     factors_name='IF_factors',
+    #     dataset=train_dataloader
+    # )
 
-    # Compute all pairwise influence scores with the computed factors.
-    analyzer.compute_pairwise_scores(
-        scores_name="IF_scores",
-        factors_name="IF_factors",
-        query_dataset=valid_dataloader,
-        train_dataset=train_dataloader,
-        per_device_query_batch_size=1024,
-    )
+    # # Compute all pairwise influence scores with the computed factors.
+    # analyzer.compute_pairwise_scores(
+    #     scores_name="IF_scores",
+    #     factors_name="IF_factors",
+    #     query_dataset=valid_dataloader,
+    #     train_dataset=train_dataloader,
+    #     per_device_query_batch_size=1024,
+    # )
     
-    # Load the scores with dimension `len(eval_dataset) x len(train_dataset)`.
-    scores = analyzer.load_pairwise_scores(scores_name="my_scores")
+    # # Load the scores with dimension `len(eval_dataset) x len(train_dataset)`.
+    # scores = analyzer.load_pairwise_scores(scores_name="my_scores")
     
-    # IF value for individual validation sample can be obtained as,
-    # sample_score = scores[index]
+    # # IF value for individual validation sample can be obtained as,
+    # # sample_score = scores[index]
 
-    lora_r = 8
-    lora_alpha = 16
-    lora_dropout = 0.05
-    # lora_target_modules
-    lora_target_modules = [
-        'q_proj', 'v_proj', 'k_proj', 'o_proj',
-        'gate_proj', 'down_proj', 'up_proj'
-    ]
+    # lora_r = 8
+    # lora_alpha = 16
+    # lora_dropout = 0.05
+    # # lora_target_modules
+    # lora_target_modules = [
+    #     'q_proj', 'v_proj', 'k_proj', 'o_proj',
+    #     'gate_proj', 'down_proj', 'up_proj'
+    # ]
 
-    # create the config object
-    peft_config = LoraConfig(
-        r=lora_r,
-        lora_alpha=lora_alpha,
-        lora_dropout=lora_dropout,
-        target_modules=lora_target_modules,
-        task_type='CAUSAL_LM' # This is import for a model like Mistral
-    )
+    # # create the config object
+    # peft_config = LoraConfig(
+    #     r=lora_r,
+    #     lora_alpha=lora_alpha,
+    #     lora_dropout=lora_dropout,
+    #     target_modules=lora_target_modules,
+    #     task_type='CAUSAL_LM' # This is import for a model like Mistral
+    # )
 
-    # create the PEFT model
-    model = get_peft_model(model, peft_config)
+    # # create the PEFT model
+    # model = get_peft_model(model, peft_config)
 
-    # Print the number of trainable parameters
-    model.print_trainable_parameters()
+    # # Print the number of trainable parameters
+    # model.print_trainable_parameters()
 
+    layerIFs = util.get_IF()
+    budget = 4 * len(layerIFs)
+    # layer_quality = [math.sqrt(value) for value in layerIFs]
+    lambda_avg, e_l = logUtility(budget, layer_quality=layerIFs)
+    print(f"lambda_avg: {lambda_avg}")
+    print(f"e_l: {e_l}")
 
     
 
--- mdl/util.py
+++ mdl/util.py
@@ -1,7 +1,16 @@
 import json
 import os
 import pickle as pkl
+import numpy as np
 
-if __name__ == '__main__':
-    path = '../Expert_Allocation'
+def get_IF():
+    path = '/data/mdl-layerIF/Expert_Allocation/layerIF_Computation/outputs/layerIF_values/mistral-7B'
+    layer_IFs = []
+    for file in sorted(os.listdir(path)):
+        if file.endswith('pkl'):
+            results = pkl.load(open(os.path.join(path, file), 'rb'))
+            layerIF_value = results['influence']['proposed'].to_numpy()
+            layer_IFs.append(-1.0*np.sum(layerIF_value))
+    return layer_IFs
+
     
